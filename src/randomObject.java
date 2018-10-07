be7144ea83d1f80c1149dc6769f65c8a31378b4b
Davidah121 <42045018+Davidah121@users.noreply.github.com>
2018-10-06 21:42:50 -0400

    No more errors
    Fixed an error with one object that is not needed anyway.

--- src/randomObject.java
+++ src/randomObject.java
@@ -11,8 +11,8 @@ public class randomObject extends parentGameObject {
 	@Override
 	public void draw() {
 		// TODO Auto-generated method stub
-		Mat4f t = GameMath.createViewMatrix(position.x, position.y, 0, 0, 0, 0, GameMath.TYPE_2D);
-		Game.setOrthoMatrix(m);
+		//Mat4f t = GameMath.createViewMatrix(position.x, position.y, 0, 0, 0, 0, GameMath.TYPE_2D);
+		//Game.setOrthoMatrix(m);
 		
 		GameRender.drawTextStretched("This is whatever this is", 320, 240, 400, 200);
 		GameRender.drawCircle(0, 0, 32, true);
